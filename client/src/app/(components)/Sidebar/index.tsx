ad454752458c0fb453744dbab2db50c9f02fd7d8
Ng.Ph Phuong Anh <pham-phuong-anh.nguyen@informatik.hs-fulda.de>
2025-01-12 00:32:27 +0100

    Created NavbarUI and SidebarUI

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,30 @@
+import { Menu } from "lucide-react";
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* TOP LOGO */}
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>logo</div>
+        <h1 className="font-extrabold text-2xl">PASTOCK</h1>
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+
+      {/* LINKs */}
+      <div className="flex-grow mt-8">
+        {/* links here */}
+      </div>
+
+      {/* FOOTER */}
+      <div className="text-center text-xs text-gray-500">&copy; 2024 PASTOCK </div>
+    </div>
+  );
+};
+
+export default Sidebar;
