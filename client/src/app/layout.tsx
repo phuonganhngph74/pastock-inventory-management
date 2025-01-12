ab76a0fb49590903f069f8358602fe15263d044e
Ng.Ph Phuong Anh <pham-phuong-anh.nguyen@informatik.hs-fulda.de>
2025-01-12 10:51:19 +0100

    Created dashboard page first UI

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import DashboardWrapper from "./dashboardWrapper";
+import DashboardWrapper from "./DashboardWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
